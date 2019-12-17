d76499f4aa9e9811c91351680c39c73d60216ea1
Hunter <hmansanas.aie@gmail.com>
2019-12-17 14:48:26 -0800

    UI Start Menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuScript : MonoBehaviour
+{
+    public bool isPaused = false;
+    public bool isPressed = false;
+    public Canvas startMenu;
+    public Button playButton;
+    public Slider volume;
+    public Button exitButton;
+    public Text exitText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startMenu = startMenu.GetComponent<Canvas>();
+        playButton = playButton.GetComponent<Button>();
+
+
+        exitButton.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /* public void LoadLeve()
+     {
+         Application.LoadLevel("");
+     }*/
+
+    public void ExitPress()
+    {
+        exitButton.enabled = true;
+        playButton.enabled = false;
+
+    }
+    public void NoPress()
+    {
+        exitButton.enabled = false;
+        playButton.enabled = true;
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
