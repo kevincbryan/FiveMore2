e69cbc3473455664f14a4efcca76e20e089af159
Hunter <hmansanas.aie@gmail.com>
2019-12-17 17:36:25 -0800

    UI Work

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
     public bool isPaused = false;
     public bool isPressed = false;
     public Canvas startMenu;
+    public GameObject startMennu;
     public Button playButton;
     public Slider volume;
     public Button exitButton;
-    public Text exitText;
+    public GameObject exitText;
+  
+    
+    
+
+
+    public AudioMixer mixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
 
 
     // Start is called before the first frame update
@@ -20,36 +33,37 @@ public class MenuScript : MonoBehaviour
     {
         startMenu = startMenu.GetComponent<Canvas>();
         playButton = playButton.GetComponent<Button>();
+        exitText = GameObject.Find("exitBox").GetComponent<GameObject>();
 
-
-        exitButton.enabled = false;
+        exitText.SetActive(false);
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-    /* public void LoadLeve()
+    public void LoadLeve()
      {
-         Application.LoadLevel("");
-     }*/
+        SceneManager.LoadScene("MainGame");
+        Destroy(startMennu);
+     }
 
     public void ExitPress()
-    {
-        exitButton.enabled = true;
-        playButton.enabled = false;
-
+    { 
+        exitText.SetActive(true);
+        
     }
     public void NoPress()
     {
-        exitButton.enabled = false;
-        playButton.enabled = true;
+        exitText.SetActive(false);
     }
     public void ExitGame()
     {
         Application.Quit();
+        
     }
 
 }
